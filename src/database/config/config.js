de7657dbd6a74fef338c690789286243a15120ef
Gus <gus_eze1988@hotmail.com>
2023-03-16 00:36:30 -0300

    Agrega config y env example

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,11 @@
 module.export = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
+    username: "root", 
+    password: "root", // Password al momento de instalarlo
+    database: "kitchening_db", // Nombre de la base de datos con la que nos vamos a conectar
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "mysql", // Lenguaje
+    port: 3306, //Puerto
   },
   test: {
     username: "root",
