accbf34a8d266d694bd494574c82c9be85ea5406
Gus <gus_eze1988@hotmail.com>
2023-04-07 23:58:20 -0300

    clase 94 y 95 terminadas

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,17 +1,40 @@
 const courses = require('../data/courses.json');
+const {Op} = require("sequelize");
+const db = require("../database/models");
 
 module.exports = {
     home : (req, res) => {
         /* toda la lógica!!! */
-        const newCourses = courses.filter(course => course.newest);
-        const saleCourses = courses.filter(course => course.sale);
-
-        return res.render('home',{
-          title : "Kitchening | HOME",
-          courses,
-          newCourses,
-          saleCourses
+        const newCourses = db.Course.findAll({
+            order : [["createdAt", "DESC"]],
+            limit : 4,
+            include :  ["images"]
         });
+
+        const saleCourses = db.Course.findAll({
+          where : {
+              discount : {
+                [Op.ne] : 0,
+              },
+          },
+          include :  ["images"]
+        });
+
+        const courses = db.Course.findAll({
+          include :  ["images"],
+        });
+
+        Promise.all([newCourses, saleCourses, courses])
+          .then(([newCourses, saleCourses, courses]) => {
+            return res.render('home',{
+              title : "Kitchening | HOME",
+              courses,
+              newCourses,
+              saleCourses
+            });
+          }) // Recibe un callback, por eso va dentro de otro parentesis. 
+          .catch(error => console.log(error))
+        
       },
     admin : (req,res) => {
       return res.render('dashboard',{
